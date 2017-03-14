07e4c77324163126d3d14a91938ed0038930d2b2
Andrew Baine <andrew.baine@gmail.com>
2017-03-14 16:11:54 -0400

    implement webapp

--- messaging/webapp.py
+++ messaging/webapp.py
@@ -2,22 +2,46 @@
 from flask import Flask
 from flask import request
 
+import argparse
+import binascii
 import json
 import logging
+import subprocess
 import time
 import thread
 
 app = Flask(__name__)
 
+context = {
+    'transactions': {}
+}
 
-def poll ():
+def poll_for_incoming():
     while True:
         logging.info('polling')
-        time.sleep(5)
-
-context = {
-
-}
+        t = int(round(time.time()))
+        output = subprocess.check_output(['zcash-cli', 'z_listreceivedbyaddress', context['z'], '0'])
+        transactions = json.loads(output)
+        for tx in transactions:
+            txid = tx['txid']
+            memo = tx['memo']
+            if txid in context['transactions']:
+                pass
+            else:
+                logging.info('new transaction %s', txid)
+                try:
+                    end = len(memo)
+                    while (end > 1) and (memo[end - 2: end] == '00'):
+                        end -= 2
+                    memo = binascii.unhexlify(memo[0:end])
+                    logging.info('parsed memo: %s', memo)
+                    d = json.loads(memo)
+                    logging.info('json memo: %s', d)
+                    context['transactions'][txid] = (t, memo)
+                except Exception as e:
+                    logging.exception(e)
+                    context['transactions'][txid] = (t, None)
+        time.sleep(10)
 
 @app.route("/")
 def hello():
@@ -25,20 +49,53 @@ def hello():
 
 @app.route('/messages')
 def get_messages():
-    seq = request.args.get('seq')
-    return seq
+    seq = int(request.args.get('seq'))
+    new_seq = 0
+    messages = []
+    for k,v in context['transactions'].items():
+        (t, memo) = v
+        if t > new_seq:
+            new_seq = t
+        if memo is not None and t > seq:
+            messages.append(memo)
+    return json.dumps({
+        'seq': new_seq,
+        'messages': messages
+    })
 
-@app.route('/messages/send/<z>', methods=['POST'])
-def add_message(z):
-    message = request.get_json()
-    body = json.dumps(message)
-    print z
-    logging.info('sending to %s: %s', z, message)
-    # todo: actually send the message
+@app.route('/messages/send/<to_address>', methods=['POST'])
+def send_message(to_address):
+    from_address = context['z']
+    rawMemo = json.dumps(request.get_json())
+    memo = binascii.hexlify(rawMemo)
+    transactions = [{
+        'address': to_address,
+        'memo': memo,
+        'amount': 0.01
+    }]
+    transaction_text = json.dumps(transactions)
+    minconf = '0'
+    logging.info('sending %s', transaction_text)
+    output = subprocess.check_output([
+        'zcash-cli',
+        'z_sendmany',
+        from_address,
+        transaction_text,
+        minconf
+    ])
+    logging.info('result: %s',  output)
     return 'message sent'
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    thread.start_new_thread(poll, ())
-    app.run()
-    logging.info('app is running')
+     parser = argparse.ArgumentParser()
+     parser.add_argument('--z')
+     args = parser.parse_args()
+     if not args.z:
+         raise Error('please pass --z')
+     else:
+         context['z'] = args.z
+
+     logging.basicConfig(level=logging.INFO)
+     thread.start_new_thread(poll_for_incoming, ())
+     app.run()
+     logging.info('app is running')
